bbcfa74d0e903ad2a5bca436dc7ee2d916c7c187
Rudro Dhar <rudro149187@bsdi-bd.org>
2024-06-11 02:56:42 +0600

    display product by category

--- store/views.py
+++ store/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
+from category.models import Category 
 
-def store(request):
-    products = Product.objects.all().filter(is_avilable = True)
-    product_count = products.count()
+def store(request, category_slug=None):
+    catogries = None
+    products = None
+    if category_slug != None:
+        catogries = get_object_or_404(Category, slug = category_slug)
+        products = Product.objects.all().filter(category = catogries, is_avilable = True)
+        product_count = products.count()
+
+    else:
+        products = Product.objects.all().filter(is_avilable = True)
+        product_count = products.count()
+
     context = {
         'products' : products,
         'product_count' : product_count
