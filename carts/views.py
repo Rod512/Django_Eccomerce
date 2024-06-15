211c74c88da94f0422859cfbad02a417babc57da
Rudro Dhar <rudro149187@bsdi-bd.org>
2024-06-15 14:38:41 +0600

    cart remove

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from store.models import Product
 from .models import Cart,CartItem
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -39,6 +39,26 @@ def add_cart(request,product_id):
         cart_item.save()
 
     return redirect('cart')
+
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request,product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product,cart=cart)
+
+    cart_item.delete()
+    return redirect('cart')
    
 
 
@@ -49,7 +69,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        gst = (18 * total) / 100
+        gst = (4 * total) / 100
         grand_total = total + gst
     except ObjectDoesNotExist :
         pass
