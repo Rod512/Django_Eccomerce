15d7ea2a9f6a9dea8f84e990e6f0fd5bdc8c5844
Rod512 <rudro149187@bsdi-bd.org>
2024-07-08 20:19:36 +0530

    Account activation encode

--- account/views.py
+++ account/views.py
@@ -4,6 +4,12 @@ from .models import Account
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator 
+from django.core.mail import EmailMessage
+
 
 
 
@@ -20,6 +26,18 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username = username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            current_site = get_current_site(request)
+            mail_subject = messages('please activate your account')
+            message = render_to_string('account/account_verification_email.html',{
+                'user' : user,
+                'domain' : current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token.make_token(user)
+            })
+            to_mail = email
+            send_mail = EmailMessage(mail_subject, message, to=[to_mail])
+            send_mail.send()
             messages.success(request, 'registration successfull')
             return redirect('register')
 
