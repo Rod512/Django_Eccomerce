076f7a5d4c12498e2cba852aa987ca03245a348b
Rod512 <rudro149187@bsdi-bd.org>
2024-07-09 00:42:32 +0530

    smtp configure

--- account/views.py
+++ account/views.py
@@ -6,7 +6,8 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator 
 from django.core.mail import EmailMessage
 
