04cfc74a4866066b11f743c0eabf9a9df5757ec9
Rod512 <rudro149187@bsdi-bd.org>
2024-07-20 02:15:38 +0530

    django form for checkout

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from carts.models import CartItem
+from .forms import OrderForm
 
-# Create your views here.
+def place_order(request):
+    current_user = request.user
+
+    #if cart count is equal to 0 or less than zero then redirect back to shop
+    cart_items = CartItem.objects.filter(user = current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+    
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+
