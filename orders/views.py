3e4a8d9b0617caa01b5c001732d5966b030192bd
Rod512 <rudro149187@bsdi-bd.org>
2024-07-21 15:04:45 +0530

    Billig address

--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,10 @@ from .forms import OrderForm
 from .models import Order
 import datetime
 
+
+def payments(request):
+    return render(request, 'order/payments.html')
+
 def place_order(request, total=0, quantity=0,):
     current_user = request.user
 
@@ -43,7 +47,7 @@ def place_order(request, total=0, quantity=0,):
             data.tax = gst
             data.ip = request.META.get('REMOTE_ADDR')
             data.save()
-
+            
             #generate order number
             yr = int(datetime.date.today().strftime('%Y'))
             dt = int(datetime.date.today().strftime('%d'))
@@ -52,8 +56,19 @@ def place_order(request, total=0, quantity=0,):
             current_date = d.strftime('%Y%m%d')
             order_number = current_date + str(data.id)
             data.order_number = order_number
+          
             data.save()
-            return redirect('checkout')
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number,)
+
+            context ={
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'quantity': quantity,
+                'gst' : gst,
+                'grand_total' : grand_total,
+            }
+            return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
        
