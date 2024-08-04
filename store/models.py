b4aae5a398b212d140f6a49b0f329b57f93d8a95
Rod512 <rudro149187@bsdi-bd.org>
2024-08-05 03:22:16 +0530

    ratings model &  design

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from category.models import Category
 from django.urls import reverse
+from account.models import Account
 
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
@@ -47,4 +48,19 @@ class Variation(models.Model):
         return self.variation_value
 
 
-   
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+
+
+
