8bfa0cb6806454e1fd3a4bc057650358fda4d313
Rod512 <rudro149187@bsdi-bd.org>
2024-08-20 19:11:26 +0530

    dynamic product gallery

--- category/views.py
+++ category/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product,ReviewRating
+
 
 def home(request):
-    products = Product.objects.all().filter(is_avilable = True)
+    products = Product.objects.all().filter(is_avilable = True).order_by('created_date')
+    #get the review
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id= product.id, status = True)
     context = {
-        'products' : products
+        'products' : products,
+        'reviews' : reviews
     }
     return render(request, 'category/index.html',context)
--- store/views.py
+++ store/views.py
@@ -9,7 +9,7 @@ from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.db.models import Q
 from django.contrib import messages
 from django.urls import reverse
-
+from .models import ProductGallery
 
 
 def store(request, category_slug=None):
@@ -54,12 +54,17 @@ def product_detail(request,category_slug,product_slug):
     # else:
     #     orderproduct = None
 
-        context = {
-            'single_product' : single_product,
-            'in_cart'   : in_cart,
-            'orderproduct' : orderproduct,
-            'reviews' : reviews,
-        }
+    #get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id = single_product.id)
+
+
+    context = {
+        'single_product' : single_product,
+        'in_cart'   : in_cart,
+        'orderproduct' : orderproduct,
+        'reviews' : reviews,
+        'product_gallery' : product_gallery
+    }
         
     return render(request,'store/product_detail.html', context)
 
